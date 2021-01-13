0a160c197267a9a8c4f5971937430aa62932ca66
Julz <jujasen@hotmail.com>
2021-01-13 16:04:07 +0100

    fixed menu link animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,20 +14,20 @@ const Navbar = () => {
                         <img className="navbar__menu-btn" src={menuBtn} alt="menu button"></img>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="menu__nav" class="navbar-nav ms-auto">
-                            <li className="menu__item" class="nav-item">
+                        <ul class="menu__nav navbar-nav ms-auto">
+                            <li class="menu__item nav-item">
                                 <a class="menu__link menu__link--active" href="#">Home</a>
                             </li>
-                            <li className="menu__nav" class="nav-item">
+                            <li class="menu__item nav-item">
                                 <a class="menu__link" href="#">Projects</a>
                             </li>
-                            <li className="menu__nav" class="nav-item">
+                            <li class="menu__item nav-item">
                                 <a class="menu__link" href="#">About</a>
                             </li>
-                            <li className="menu__nav" class="nav-item">
+                            <li class="menu__item nav-item">
                                 <a class="menu__link" href="#">Skills</a>
                             </li>
-                            <li className="menu__nav" class="nav-item">
+                            <li class="menu__item nav-item">
                                 <a class="menu__link" href="#">Contact</a>
                             </li>
                         </ul>
