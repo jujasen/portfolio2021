648c1ae177f1157c27cacd8be5e3627a06747b63
Julz <jujasen@hotmail.com>
2021-01-14 03:23:34 +0100

    added eventlistener on project visual

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,6 +10,7 @@ const Projects = () => {
     return (
         <>
             <section class="projects">
+                <h2 class="sectionheading">Projects</h2>
                 <Project name="Venom Lips" image={venomlips} description="An e-commerce store for a fictional brand. Created with HTML, CSS, JS and Strapi API" link="https://venomlips.julz.no/" />
             </section>
         </>
--- src/components/elements/Project.js
+++ src/components/elements/Project.js
@@ -1,9 +1,35 @@
 import react from 'react';
+import React from 'react';
+import {useEffect} from 'react';
 
 const Project = (props) => {
 
     const { name, image, description, link } = props;
     console.log(props);
+
+    const handleHover = (visual, text, img) => {
+
+
+        visual.classList.add("project__visual--hidden");
+        text.classList.remove("project__title");
+        text.classList.add("project__title--hidden");
+        img.classList.remove("project__img--hidden");
+        img.classList.add("project__img");
+    };
+
+    React.useEffect(() => {
+        const visual = document.querySelector(".project__visual");
+        const visualText = document.querySelector(".project__title");
+        const visualImg = document.querySelector(".project__img--hidden");
+
+        visual.addEventListener('mouseover', function() {
+            handleHover(visual, visualText, visualImg)
+        });
+
+        return () => {
+            visual.removeEventListener('scroll', handleHover);
+        };
+    }, []);
 
 
 
@@ -11,16 +37,18 @@ const Project = (props) => {
     return (
         <>
             <div class="project">
-                <div class="project__section">
+                <div class="project__section project__visual">
                     <h3 class="project__title">{name}</h3>
-                    <img class="project__img" src={image} alt={name + " screenshot"}></img>
                 </div>
+                <img class="project__img--hidden" src={image} alt={name + " screenshot"}></img>
                 <div class="project__section">
                     <p class="project__desc">{description}</p>
-                    <a href={link}></a>
+                    <a class="project__link" href={link}>View &gt;&gt;</a>
                 </div>
             </div>
         </>
+
+        
     )
 }
 
