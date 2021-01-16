6b79cdbdd4c232f06d94e674b461c834af9207cc
Julz <jujasen@hotmail.com>
2021-01-17 00:47:03 +0100

    added projects and fixed projectview

--- src/components/elements/Project.js
+++ src/components/elements/Project.js
@@ -4,7 +4,7 @@ import {useState} from 'react';
 
 const Project = (props) => {
 
-    const { name, image, description, link } = props;
+    const { id, name, image, description, link } = props;
 
     const [hovered, setHovered] = useState(false);
     const toggleHover = () => setHovered(!hovered);
@@ -12,25 +12,19 @@ const Project = (props) => {
     return (
         <>
             <div className="project">
-                <div id="carouselExampleIndicators" className="carousel slide carousel-dark" data-bs-ride="carousel">
+                <div id={id} className="carousel slide carousel-dark" data-bs-ride="carousel">
                     <div className="carousel-inner">
                         <div className="carousel-item active">
                             <div className="project__visual">
-                                <h3 className="project__title">{name}</h3>
+                                <a href={link}><h3 className="project__title">{name}</h3></a>
                             </div>
                         </div>
                         <div className="carousel-item">
+                            <a href={link}>
                             <img src={image} className="project__img d-block w-100" alt={name + " screenshot"} />
+                            </a>
                         </div>
                     </div>
-                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
-                        <span className="project__nav carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </a>
-                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
-                        <span className="project__nav carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </a>
                 </div>
                 <div className="project__section">
                     <p className="project__desc">{description}</p>
