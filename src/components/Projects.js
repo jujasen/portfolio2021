3e32591f5e17bd5046cd789326172abf24217b45
Julz <jujasen@hotmail.com>
2021-01-20 16:10:27 +0100

    added desktop styles for contact and navbar

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,7 +18,7 @@ const Projects = () => {
 
 
                 <Project id="1" name="Venom Lips" image={venomlips} description="An e-commerce store for a fictional brand. Created with HTML, CSS, JS and Strapi API" link="https://venomlips.julz.no/" />
-                <Project id="2" name="Dartmoor" image={dartmoor} description="A redesign of informational page for Dartmoor. Made in Figma." link="https://www.figma.com/proto/D2XEQSTbAHBmAMct9V62SO/Untitled?node-id=1%3A10&viewport=-803%2C25%2C0.5462504029273987&scaling=scale-down-width" />
+                <Project id="2" name="Dartmoor" image={dartmoor} description="A redesign of an informational page for Dartmoor. Made in Figma." link="https://www.figma.com/proto/D2XEQSTbAHBmAMct9V62SO/Untitled?node-id=1%3A10&viewport=-803%2C25%2C0.5462504029273987&scaling=scale-down-width" />
                 <Project id="3" name="BT Museum" image={bergentekniske} description="A redesign of the webpage for Bergen Technical Museum. Made in Figma." link="https://www.figma.com/proto/o9ojcemXyg5RNzYl0WuzNc/Untitled?node-id=1%3A2&viewport=789%2C862%2C0.21653564274311066&scaling=scale-down-width" />
                 <Project id="4" name="Flower Power" image={flower} description="An e-commerce store created with Wordpress." link="https://flowerpower.julz.no/" />
                 <Project id="5" name="Explore Mars" image={mars} description="A landingpage for weather data and NASA's work with Mars." link="https://mars.julz.no/" />
