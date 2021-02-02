8c687c747e35b36121b34073842612595337dc87
Julz <jujasen@hotmail.com>
2021-02-03 00:22:23 +0100

    added lovebird project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,14 +7,18 @@ import mars from '../images/projectimg/mars.png'
 import flower from '../images/projectimg/flower.png'
 import nicecream from '../images/projectimg/nicecream.png'
 import coownlink from '../images/coown-present.pdf'
+import lovebird from '../images/projectimg/lovebird.png'
 
 const Projects = () => {
+
+    console.log(lovebird, nicecream);
     
     return (
         <>
             <section className="projects">
                 <h2 className="sectionheading">Projects</h2>
 
+                <Project id="8" name="Love Bird" image={lovebird} description="A cute-ified version of Flappy Bird. My first try making a game. Desktop only. Created with HTML, CSS and JS." link="https://lovebird.julz.no/" />
                 <Project id="7" name="Venom Lips" image={venomlips} description="An e-commerce store for a fictional brand. Created with HTML, CSS, JS and Strapi API" link="https://venomlips.julz.no/" />
                 <Project id="6" name="Dartmoor" image={dartmoor} description="A redesign of an informational page for Dartmoor. Made in Figma." link="https://www.figma.com/proto/D2XEQSTbAHBmAMct9V62SO/Untitled?node-id=1%3A10&viewport=-803%2C25%2C0.5462504029273987&scaling=scale-down-width" />
                 <Project id="5" name="Co-Own" image={coown} description="Web-design for the start-up Co-Own. Made in Figma, presented in pdf-form." link={coownlink} />
